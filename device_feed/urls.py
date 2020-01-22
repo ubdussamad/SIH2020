5095903c23e318961d1ee5bfbe3b01dd60df53f4
Nishant Ranjan <darkfist1998@gmail.com>
2020-01-22 14:11:10 +0530

    Almost Finished Prototype

--- device_feed/urls.py
+++ device_feed/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
+from django.conf.urls import url
 from . import views
 
 urlpatterns = [
     path('', views.home, name='device_feed-home'),
+    path('login/', views.login, name='device_feed-login'),
+    # path('output/', views.auth),
+    url(r'^login/output/$',views.auth),
 ]
--- device_feed/views.py
+++ device_feed/views.py
@@ -1,22 +1,32 @@
 from django.shortcuts import render
 from django.conf import settings
+from django.http import HttpResponse, HttpResponseRedirect
 import json
 import os
 
 
 def home(request):
-    # jsonFile = open(os.path.join(settings.STATIC_ROOT, 'data.json' )).read()
-    # jsonData = json.loads(jsonFile)
-    # for i in range(0, len(jsonData)):
-    #     if (i == len(jsonData)-1):
-    #         context = jsonData[i]
-
-    context = {
-        'deviceID':'123',
-        'currentLevel': '22',
-        'maxLevel': '2',
-        'minLevel': '2'
-    }
+    jsonFile = open(os.path.join(settings.STATIC_ROOT, 'data.json' )).read()
+    jsonData = json.loads(jsonFile)
+    for i in range(0, len(jsonData)):
+        if (i == len(jsonData)-1):
+            context = jsonData[i]
     
+    # if (context['currentLevel'] == '2'):
+    #     level2 = True
+    # if (context['currentLevel'] == '1'):
+    #     level1 = True
+        
     return render(request, "device_feed/index.html", context)
 
+def login(request):
+    context = {}
+    return render(request, "device_feed/login.html", context)
+
+def auth(request):
+    username_default = request.POST['username']
+    password_default = request.POST['password']
+    if (username_default == "admin"):
+        if (password_default == "admin"):
+            print("success")
+            return HttpResponseRedirect('http://127.0.0.1:8000/')
